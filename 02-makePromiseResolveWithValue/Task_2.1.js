04907e07e96932157adca4519a44f4843db6b61c
Гайсюк Вілгельм 141 (2-1) <haisiuk.vilhelm@chnu.edu.ua>
2026-03-03 17:51:13 +0200

    task 2/2.1

---
+++ 02-makePromiseResolveWithValue/Task_2.1.js
@@ -0,0 +1,16 @@
+// ==================== ЗАВДАННЯ 2.1 ====================
+/**
+ * Створіть проміс, який одразу резолвиться зі значенням
+ *
+ * @param {any} value - Будь-яке значення
+ * @returns {Promise<any>}
+ */
+function makePromiseResolveWith(value) {
+    return new Promise ((resolve) => {
+    resolve(value)
+    })
+}
+
+// Перевірка:
+makePromiseResolveWith(5)
+    .then(value => console.log(' Тест 2.1:', value)); // Очікується: 5
