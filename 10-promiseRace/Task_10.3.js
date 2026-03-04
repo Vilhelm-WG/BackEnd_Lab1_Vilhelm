3d047e5bf4d302086f6ac5ae6b6742a80a3b15d8
Гайсюк Вілгельм 141 (2-1) <haisiuk.vilhelm@chnu.edu.ua>
2026-03-04 16:52:58 +0200

    task 4/10.3

---
+++ 10-promiseRace/Task_10.3.js
@@ -0,0 +1,34 @@
+// ==================== ЗАВДАННЯ 10.3 ====================
+/**
+ * Симуляція запитів до різних серверів
+ * Поверніть відповідь від найшвидшого сервера
+ */
+
+function fetchFromServer(serverName, delay) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve({
+                server: serverName,
+                data: `Data from ${serverName}`,
+                responseTime: delay
+            });
+        }, delay);
+    });
+}
+
+/**
+ * Отримайте дані від найшвидшого сервера
+ *
+ * @returns {Promise<{server: string, data: string, responseTime: number}>}
+ */
+function fetchFromFastestServer() {
+    // TODO: Створіть запити до трьох серверів з різними затримками
+    // Server A: 1000ms, Server B: 500ms, Server C: 800ms
+    // Поверніть результат від найшвидшого
+}
+
+// Перевірка:
+fetchFromFastestServer()
+    .then(result => console.log(' Тест 10.3:', result));
+// Очікується: { server: 'Server B', data: 'Data from Server B', responseTime: 500 }
+
